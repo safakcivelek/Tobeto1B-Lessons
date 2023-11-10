66a3b9e9c4ff539851dceebc00889363084cda22
safakcivelek <safakcivelek3@gmail.com>
2023-11-10 20:52:51 +0300

    .

---
+++ CSharp/9.Oturum/TobetoCourseAcademy/Business/Concretes/CategoryManager.cs
@@ -0,0 +1,15 @@
+﻿using DataAccess.Concretes;
+using Entities.Concretes;
+
+namespace Business.Concretes
+{
+    public class CategoryManager
+    {
+        public void Add(Category category)
+        {
+            AdoNetCategoryDal categoryDal = new AdoNetCategoryDal();
+
+        }
+    }
+    
+}
---
+++ CSharp/9.Oturum/TobetoCourseAcademy/Entities/Concretes/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concretes
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
+//Sıfırdan proje (solution) oluşturunuz-->TobetoETrade //Product sınıfını katmanlarında işletiniz. //Product -->Id,Name,UnitPrice
+//program.cs de mutlaka veri girişleri yapınız.
+//tüm pairler bitmeden sonraki aşamaya geçmek yok :)
+//tüm pairler bitince 10 dk mola.
+//İkinci aşama. Önceki derste (çarşamba) oluşturduğumuz 4 varlığı (entity) //TobetoCourseAcademy projesine uygulayınız. //program.cs de mutlaka veri girişleri yapınız.
