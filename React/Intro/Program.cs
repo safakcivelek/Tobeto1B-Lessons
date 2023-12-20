58e3e1f8d9574d82ddd1ac2301de6623244436cc
safakcivelek <safakcivelek3@gmail.com>
2023-12-20 14:45:08 +0300

    React Folder  Added

---
+++ React/Intro/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//React Giriş!!!
+//React derslerine geçiş yapıldı.
+//React ders içerikleri,projeleri ilgili klasör altına eklenecektir.
